189fd7a09e1f12033b40d4cebb04bb8a2527870d
AntoninPutala <165297161+AntoninPutala@users.noreply.github.com>
2024-11-14 12:57:57 +0100

    Add files via upload

---
+++ Test_ser_port.py
@@ -0,0 +1,44 @@
+
+import serial
+import time
+
+def delay_microseconds(microseconds):
+    start_time = time.perf_counter()
+    end_time = start_time + microseconds / 1_000_000
+    while time.perf_counter() < end_time:
+        pass
+
+# Open serial port
+uart = serial.Serial('COM3', baudrate=115200,timeout=1)  # Adjust 'COM1' and baud rate as needed
+uart.bytesize = serial.EIGHTBITS
+uart.stopbits = serial.STOPBITS_ONE
+uart.parity = serial.PARITY_NONE
+
+time.sleep(1)
+
+data = "Bye!"
+data_bytes = [ord(char) for char in data]
+
+# Write data to the serial port
+for byte in data_bytes:
+    uart.write(byte.to_bytes(1,'big'))
+    delay_microseconds(200)
+
+# Read data from the serial port
+time.sleep(3)
+
+response = uart.read(100)  # Read up to 100 bytes
+
+# Print the response
+print(response)
+
+# Close the serial port
+#uart.close()
+
+#≡print(data_bytes)
+
+
+
+
+
+
