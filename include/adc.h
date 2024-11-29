33edc216502156d254b2073a2cc9c17c41a6ab51
AntoninPutala <165297161+AntoninPutala@users.noreply.github.com>
2024-11-29 15:07:12 +0100

    Add files via upload

--- include/adc.h
+++ include/adc.h
@@ -1,82 +1,53 @@
 #ifndef ADC_H
 # define ADC_H
 
+/***********************************************************
+ * 
+ * Timer library for AVR-GCC.
+ * (c) 2024 Jakub Kupcik, MIT license
+ *
+ * Developed using PlatformIO and AVR 8-bit Toolchain 3.6.2.
+ * Tested on Arduino Uno board and ATmega328P, 16 MHz.
+ *
+ ***********************************************************/
+
+/**
+ * @file 
+ * @defgroup kupcik_adc ADC Library <adc.h>
+ * @code #include <adc.h> @endcode
+ *
+ * @brief Timer library for AVR-GCC.
+ *
+ * The library contains macros for controlling the adc modules.
+ *
+ * @note Based on Microchip Atmel ATmega328P manual and no source file
+ *       is needed for the library.
+ * @author Jakub Kupcik, Dept. of Radio Electronics, Brno University 
+ *         of Technology, Czechia
+ * @copyright (c) 2024 Jakub Kupcik, This work is licensed under 
+ *                the terms of the MIT license
+ * @{
+ */
+
+/* Defines ------------------------------------------------*/
+/**
+ * @name  Definitions for ADC.
+ * @note  ADC can switch between analog pin 2 and pin 3.
+ */
+/** @brief Start conversion*/
 #define ADC_startconversion() ADCSRA |= (1 << ADSC);
 
+/** @brief   Initializate ADC .
+ *  @details Multiplexer is defaultly set to analog pin 2.
+*/
 #define ADC_setup() \
     (ADCSRA |= (1 << ADPS2) | (1 << ADPS1)); \
     (ADMUX |= (1 << REFS0) | (1 << MUX1));   \
     (ADMUX &= ~((1 << MUX0) | (1 << MUX2) | (1 << MUX3))); \
     (ADCSRA |= (1 << ADEN) | (1 << ADIE));
 
+/** @brief Switch between analog pin 2 and 3 */
 #define ADC_switchport() ADMUX ^= (1 << MUX0);
 
-
+/** @} */
 #endif
-
-
-
-
-/*
-This section is to be added at the start of the program to load AVR.h and define variables.
-
-#include "AVR.h"
-
-volatile uint16_t data = 0;
-volatile uint16_t data_5 = 0;
-volatile uint16_t data_4 = 0;
-volatile uint8_t reset = 0;
-volatile uint8_t counter = 0;
-
-*/
-
-/*
-This section is to be added to main function at the start to setup timer and ADC and enable interrupts.
-
-  TIM2_ovf_16ms(); 
-  TIM2_ovf_enable();
-
-  setupADC();
-
-  sei();
-
-*/
-
-/*
-This section is to be added after main function to setup interupts for both timer and ADC.
-
-ISR(TIMER2_OVF_vect)
-{
-    counter++;
-
-    if (counter >= 6) 
-    {
-        counter = 0;
-        reset = 1;
-        switchport();
-        startconversion();
-    }
-}
-
-ISR(ADC_vect)
-{
-  data = ADC;  // Save the ADC value
-}
-
-
-*/
-
-/*
-This section is to be added to main function to an infinite loop to handle spliting the data from 2 ports and save them into a separate int.
-
-    if (reset == 1)
-    {
-        if (ADMUX & (1 << MUX0)) {
-        data_5 = data;
-        } else {
-        data_4 = data;
-        }
-    }
-  }
-
-*/
